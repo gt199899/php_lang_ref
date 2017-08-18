8b3814a89f255559aaf2b6d6014fcc8af1a654ab
gt199899 <gt199899@gmail.com>
2017-08-18 11:48:05 +0800

    update

--- Language_Reference/Functions/Anonymous_functions.php
+++ Language_Reference/Functions/Anonymous_functions.php
@@ -366,8 +366,8 @@ $cl2 = function() {
     return $this->ifoo;
 };
 
-$bcl1 = Closure::bind($cl1, null, 'A');
-$bcl2 = Closure::bind($cl2, new A(), 'A');
+$bcl1 = \Closure::bind($cl1, null, 'A');
+$bcl2 = \Closure::bind($cl2, new A(), 'A');
 echo $bcl1(), "\n";
 echo $bcl2(), "\n";
 
